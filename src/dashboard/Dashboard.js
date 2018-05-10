5c5cefd11457e5464e5f3007ce19945e0ddbf9ea
Chris Brouwer <chris.brouwer@gmail.com>
2018-05-10 13:35:53 +0200

    current consumption and formatting

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
-import { Button,Table } from 'reactstrap';
+import { Alert,Table } from 'reactstrap';
 import axios from 'axios';
 import Moment from 'moment';
+import '../App.css';
+
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = {text: '', data: []};
+    this.state = {text: 'Button', monthData: [], current: '', currentTs: Moment()};
     this.processData = this.processData.bind(this);
   }
-
-
 
   componentDidMount() {
     var context = this;
     axios.get('/api/months')
       .then(function (response) {
-        context.setState({text: response.data.express})
         context.processData(response.data.docs);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    axios.get('/api/current')
+      .then(function (response) {
+        context.setState({
+            current: response.data.consumption,
+            currentTs: Moment(response.data.date)});
+
       })
       .catch(function (error) {
         console.log(error);
@@ -35,37 +45,44 @@ class Dashboard extends Component {
       elem.date = date.subtract(1, 'months');
       result[this.getDateKey(date)] = elem
     }
-    this.setState({data: result})
+    this.setState({monthData: result})
   }
 
   getDateKey(date) {
     return date.format('YMM');
   }
-  formatKwhNumber(number) {
+
+
+  formatKwhNumber(number,precision=0) {
     if (!number) return '-'
-    return number.toFixed(0) + ' kWh'
+    return number.toFixed(precision)  + ' kWh';
+  }
+  formatDiffKwhNumber(number) {
+    if (!number) return <div>-</div>
+    const className = number > -1 ? 'Consumption-pos' : 'Consumption-neg';
+    return <div className={className}> {this.formatKwhNumber(number)} </div>
   }
 
   getTableData() {
     var context = this;
-    return Object.keys(this.state.data).sort().reverse().map(function(key, index) {
-      const elem = context.state.data[key];
+    return Object.keys(this.state.monthData).sort().reverse().map(function(key, index) {
+      const elem = context.state.monthData[key];
       var elemDate = Moment(elem.date);
       const lastMonthKey = context.getDateKey(elemDate.subtract(1, 'months'));
-      const lastMonthElem = context.state.data[lastMonthKey];
+      const lastMonthElem = context.state.monthData[lastMonthKey];
       const lastMonthDiff = lastMonthElem ? ( elem.d_total - lastMonthElem.d_total ) : NaN;
 
       var elemDate = Moment(elem.date);
       const lastYearKey = context.getDateKey(elemDate.subtract(1, 'years'));
-      const lastYearElem = context.state.data[lastYearKey];
+      const lastYearElem = context.state.monthData[lastYearKey];
       const lastYearDiff = lastYearElem ? (elem.d_total - lastYearElem.d_total ) : NaN;
 
 
       return (<tr key={key}>
         <th scope="row">{elem.date.format('MMMM Y')}</th>
         <td>{context.formatKwhNumber(elem.d_total)}</td>
-        <td>{context.formatKwhNumber(lastMonthDiff)}</td>
-        <td>{context.formatKwhNumber(lastYearDiff)}</td>
+        <td>{context.formatDiffKwhNumber(lastMonthDiff)}</td>
+        <td>{context.formatDiffKwhNumber(lastYearDiff)}</td>
       </tr>);
     })
   }
@@ -75,7 +92,9 @@ class Dashboard extends Component {
 
     return (
       <div>
-      <Button color="danger">{this.state.text}</Button>
+      <Alert color="primary">
+        {this.formatKwhNumber(this.state.current, 3)} @ {this.state.currentTs.format('HH:mm:ss')}
+      </Alert>
 
       <Table>
        <thead>
--- src/server.js
+++ src/server.js
@@ -5,6 +5,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 const dbDayCollection = 'metrics.day'
 const dbMonthCollection = 'metrics.month'
+const dbReadingCollection = 'reading'
+
+var db;
+
+MongoClient.connect(process.env.MONGO_POWERMON_URI, function(err, client) {
+
+  db = client.db();
+
+
+});
 
 
 const findMonthDocuments = function(db, callback) {
@@ -16,21 +26,28 @@ const findMonthDocuments = function(db, callback) {
   });
 }
 
+const getCurrentUsage = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection(dbReadingCollection);
+  // Find some documents
+  collection.find({}).sort({$natural: -1}).limit(1).toArray(function(err, docs) {
+    callback(docs[0]);
+  });
+}
+
+
+app.get('/api/current', (req, res) => {
+  // Use connect method to connect to the server
+  getCurrentUsage(db, function(doc) {
+    res.send({ consumption: doc.consumption, date: doc.ts });
+  });
+});
 
 app.get('/api/months', (req, res) => {
   // Use connect method to connect to the server
-  MongoClient.connect(process.env.MONGO_POWERMON_URI, function(err, client) {
-
-    const db = client.db();
-
-    findMonthDocuments(db, function(docs) {
-      res.send({ express: 'Hello From Express' , docs});
-      client.close();
-    });
-
+  findMonthDocuments(db, function(docs) {
+    res.send({ express: 'Hello From Express' , docs});
   });
-
-
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
